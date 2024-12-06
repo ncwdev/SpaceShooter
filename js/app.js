2598233f40c06de20b5008039998be4016fd8b05
ncwdev <ncwdev@outlook.com>
2024-12-06 18:06:34 +0300

    restore webgpu support

--- js/app.js
+++ js/app.js
@@ -12,8 +12,7 @@ let game = null;
 async function createEngine() {
     const options = { preserveDrawingBuffer: false, stencil: true, disableWebGL2Support: false };
 
-    // const webGPUSupported = await BABYLON.WebGPUEngine.IsSupportedAsync;
-    const webGPUSupported = false; // last chrome update broke webGPU performance
+    const webGPUSupported = await BABYLON.WebGPUEngine.IsSupportedAsync;
     if (webGPUSupported) {
         engine = new BABYLON.WebGPUEngine(canvas, options);
         await engine.initAsync();
