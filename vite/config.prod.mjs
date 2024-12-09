f990d56eee614d99e866880474f04d447dc3f145
ncwdev <ncwdev@outlook.com>
2024-12-09 23:20:06 +0300

    fix urls in prod build

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,6 +1,27 @@
 import { defineConfig } from 'vite';
 
+const repoName = '/SpaceShooter/';
+
+function replaceUrlsPlugin() {
+    return {
+        name: 'replace-urls',
+        transform(code, id) {
+            if (id.endsWith('.js')) {
+                code = code.replace(/\/images\//g, repoName + 'images/');
+                code = code.replace(/\/models\//g, repoName + 'models/');
+                code = code.replace(/\/particles\//g, repoName + 'particles/');
+                code = code.replace(/\/sounds\//g, repoName + 'sounds/');
+                return {
+                    code: code,
+                    map: null // Inline source map is not supported
+                };
+            }
+        }
+    };
+}
+
 export default defineConfig({
-    base: './',
+    base: repoName,
     publicDir: 'assets',
+    plugins: [replaceUrlsPlugin()]
 });
