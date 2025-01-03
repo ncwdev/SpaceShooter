53cdc810e31bfce8c99b334c8dd039b40c8c754c
ncwdev <ncwdev@outlook.com>
2025-01-03 23:49:02 +0300

    add host

---
+++ vite/config.host.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+
+const repoName = 'https://www.criminal-world.ru/spaceshooter/';
+
+function replaceUrlsPlugin() {
+    return {
+        name: 'replace-urls',
+        transform(code, id) {
+            if (id.endsWith('.js')) {
+                code = code.replace(/\/images\//g, repoName + 'images/');
+                code = code.replace(/\/models\//g, repoName + 'models/');
+                code = code.replace(/\/particles\//g, repoName + 'particles/');
+                code = code.replace(/\/sounds\//g, repoName + 'sounds/');
+                return {
+                    code: code,
+                    map: null // Inline source map is not supported
+                };
+            }
+        }
+    };
+}
+
+export default defineConfig({
+    base: repoName,
+    publicDir: 'assets',
+    plugins: [replaceUrlsPlugin()]
+});
